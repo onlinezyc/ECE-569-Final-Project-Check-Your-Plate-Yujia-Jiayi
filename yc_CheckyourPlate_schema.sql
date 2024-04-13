0cb1595cc2a4b77e64b538c6d9c9adfcd1e811e1
Yujia Cheng <yujia.cheng@rutgers.edu>
2024-04-13 11:28:16 -0400

    updated sql schema and added web flow diagram

--- yc_CheckyourPlate_schema.sql
+++ yc_CheckyourPlate_schema.sql
@@ -1,167 +1,250 @@
-CREATE TABLE `Dietary Reference Intakes (DRI)`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `gender_code` INT NOT NULL,
-    `age_group_code` INT NOT NULL,
-    `nutrient_code` BIGINT NOT NULL,
-    `unit` ENUM('') NOT NULL,
-    `RDA` DOUBLE(8, 2) NULL,
-    `AI` DOUBLE(8, 2) NULL,
-    `UL` DOUBLE(8, 2) NOT NULL,
-    PRIMARY KEY(`gender_code`)
+CREATE SHCEMA IF NOT EXISTS Check_your_plate;
+USE Check_your_plate;
+
+DROP TABLE IF EXISTS DRIs;
+DROP TABLE IF EXISTS User_Conditions;
+DROP TABLE IF EXISTS PhysicalConditions;
+DROP TABLE IF EXISTS User_Allergies;
+DROP TABLE IF EXISTS Substance_Categories;
+DROP TABLE IF EXISTS Food_Labels;
+DROP TABLE IF EXISTS Food_Nutrients;
+DROP TABLE IF EXISTS User_Watchlists;
+DROP TABLE IF EXISTS User_DietPrefs;
+DROP TABLE IF EXISTS Recipe_Foods;
+DROP TABLE IF EXISTS Recipe_Labels;
+DROP TABLE IF EXISTS Labels;
+DROP TABLE IF EXISTS Recipes;
+DROP TABLE IF EXISTS Substances;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Foods;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Genders;
+DROP TABLE IF EXISTS AgeGroups;
+DROP TABLE IF EXISTS Units;
+DROP TABLE IF EXISTS DietPreferences;
+
+CREATE TABLE IF NOT EXISTS Units(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(10) /* g, mg, ug, kcal*/
 );
-ALTER TABLE
-    `Dietary Reference Intakes (DRI)` ADD PRIMARY KEY(`age_group_code`);
-ALTER TABLE
-    `Dietary Reference Intakes (DRI)` ADD PRIMARY KEY(`nutrient_code`);
-CREATE TABLE `Result-references`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `result_id` BIGINT NOT NULL,
-    `ref_id` BIGINT NOT NULL,
-    PRIMARY KEY(`result_id`)
+
+CREATE TABLE IF NOT EXISTS Genders(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    gender VARCHAR (20)
 );
-ALTER TABLE
-    `Result-references` ADD PRIMARY KEY(`ref_id`);
-CREATE TABLE `User-Conditions`(
-    `id` BIGINT NOT NULL,
-    `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `condition_id` BIGINT NOT NULL,
-    PRIMARY KEY(`condition_id`)
+
+CREATE TABLE IF NOT EXISTS AgeGroups(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),  /*'infant','toddler','child','teenager','adult','elder (65+)'*/
+    description VARCHAR(100)
 );
-ALTER TABLE
-    `User-Conditions` ADD INDEX `user_conditions_id_index`(`id`);
-CREATE TABLE `References`(
-    `ref_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `source_name` VARCHAR(255) NOT NULL,
-    `authors` VARCHAR(255) NOT NULL,
-    `publication_name` VARCHAR(255) NOT NULL,
-    `publication_year` BIGINT NOT NULL
+
+CREATE TABLE IF NOT EXISTS DietPreferences(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),  /*('diary-free', 'vegetarian', 'lacto-vegetarian', 'pescatarian', 'pollotarian', 'vegan', 'gluten-free', 'keto', 'paleo')*/
+    description VARCHAR(255)
 );
-CREATE TABLE `Food Items`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `category` ENUM('') NOT NULL
+
+CREATE TABLE IF NOT EXISTS Users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(50),
+    email VARCHAR(70), 
+    UNIQUE(email),
+    gender INT,
+    ageGroup INT,
+    registrationDate datetime,
+    FOREIGN KEY (gender) REFERENCES Genders(id),
+    FOREIGN KEY (ageGroup) REFERENCES AgeGroups(id)
 );
-CREATE TABLE `User-Allergies`(
-    `id` BIGINT NOT NULL,
-    `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `allergen_id` BIGINT NOT NULL,
-    PRIMARY KEY(`allergen_id`)
+
+CREATE TABLE IF NOT EXISTS User_DietPrefs( /* Combined table of Users and DietPreferences*/ 
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+    userId INT,
+    dietId INT, 
+    FOREIGN KEY (userId) REFERENCES Users(id),
+    FOREIGN KEY (dietId) REFERENCES DietPreferences(id),
+    PRIMARY KEY (userId, dietId)
 );
-ALTER TABLE
-    `User-Allergies` ADD INDEX `user_allergies_id_index`(`id`);
-CREATE TABLE `Users`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Name` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `gender` ENUM('') NULL,
-    `age_group` ENUM('') NULL,
-    `column_7` BIGINT NOT NULL,
-    `registrationDate` DATETIME NOT NULL,
-    `diet_pref` ENUM('') NULL
+
+CREATE TABLE IF NOT EXISTS PhysicalConditions(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    description VARCHAR(255) NULL
 );
-ALTER TABLE
-    `Users` ADD UNIQUE `users_email_unique`(`email`);
-CREATE TABLE `Recipe-foods`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `recipe_id` INT NOT NULL,
-    `food_id` BIGINT NOT NULL,
-    `quantity` DOUBLE(8, 2) NOT NULL,
-    `unit` ENUM('') NOT NULL,
-    PRIMARY KEY(`recipe_id`)
+
+CREATE TABLE IF NOT EXISTS User_Conditions( /* Combined table for Users and Physical_Conditions */
+	id INT AUTO_INCREMENT,
+	UNIQUE(id),
+    userId INT,
+    conditionId INT,
+    FOREIGN KEY (userID) REFERENCES Users(id),
+	FOREIGN KEY (conditionId) REFERENCES PhysicalConditions(id),
+    PRIMARY KEY (userID, conditionId)
 );
-ALTER TABLE
-    `Recipe-foods` ADD PRIMARY KEY(`food_id`);
-CREATE TABLE `Recipes`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `description` BIGINT NOT NULL,
-    `diet_pref` ENUM('') NOT NULL,
-    `create_date` TIMESTAMP NOT NULL,
-    `last_update` TIMESTAMP NULL,
-    `autor` INT NOT NULL,
-    `updated_by` INT NULL
+
+CREATE TABLE IF NOT EXISTS Substances(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+	description VARCHAR(255) NULL,
+    allergen BOOL NULL,		/* flag if substance is reported to cause allergy */
+    toxin BOOL NULL, 		/* flag if substance is reported to be toxic */
+    MSC FLOAT NULL,     /* Maximum Saftey Concentration. Needs to be filled if the substance is a knonw toxin */
+    uint INT,
+    FOREIGN KEY (uint) REFERENCES Units(id)
 );
-CREATE TABLE `Substances`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL,
-    `category` INT NOT NULL,
-    `description` VARCHAR(255) NULL,
-    `unit` ENUM('') NOT NULL,
-    `allergen` TINYINT(1) NOT NULL,
-    `toxin` TINYINT(1) NOT NULL,
-    `MSC` DOUBLE(8, 2) NULL
+
+CREATE TABLE IF NOT EXISTS User_Allergies( /* Combined table for Users and Substances */
+	id INT AUTO_INCREMENT,
+	UNIQUE(id),
+    userId INT,
+    allergenId INT,
+    FOREIGN KEY (userID) REFERENCES Users(id),
+	FOREIGN KEY (allergenId) REFERENCES Substances(id),
+    PRIMARY KEY (userID, allergenId)
 );
-CREATE TABLE `user-watchlists`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `uid` INT NOT NULL,
-    `recipe_id` BIGINT NOT NULL
+
+CREATE TABLE IF NOT EXISTS Categories(  /* For substances classification */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(80),
+	description VARCHAR(255) NULL
 );
-CREATE TABLE `Condition Related Substances`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `condition_id` INT NOT NULL,
-    `sub_id` BIGINT NOT NULL,
-    PRIMARY KEY(`condition_id`)
+
+CREATE TABLE IF NOT EXISTS Substance_Categories( /* Combined table for Substances and Categories */
+	id INT AUTO_INCREMENT,
+	UNIQUE(id),
+	subsId INT,
+    categoryId INT,
+	FOREIGN KEY (subsId) REFERENCES Substances(id), 
+    FOREIGN KEY (categoryId) REFERENCES Categories(id), 
+    PRIMARY KEY (subsId, categoryId)
 );
-ALTER TABLE
-    `Condition Related Substances` ADD PRIMARY KEY(`sub_id`);
-CREATE TABLE `Food-nutrients`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT INDEX,
-    `food_id` BIGINT NOT NULL,
-    `sub_id` BIGINT NOT NULL
+
+CREATE TABLE IF NOT EXISTS DRIs( /* Dietary Reference Intakes */
+	id INT AUTO_INCREMENT,
+	UNIQUE(id),
+	genderId INT,
+    ageGroupId INT, 
+    subsId INT,
+    unit INT,
+    RDA float,
+    AI float,
+    UL float,
+	FOREIGN KEY (genderId) REFERENCES Genders(id), 
+	FOREIGN KEY (ageGroupId) REFERENCES AgeGroups(id), 
+	FOREIGN KEY (subsId) REFERENCES Substances(id),
+    FOREIGN KEY (unit) REFERENCES Units(id),
+    PRIMARY KEY (genderId, ageGroupId)
 );
-CREATE TABLE `Physical Conditions`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(100) NOT NULL,
-    `description` VARCHAR(255) NULL
+
+CREATE TABLE IF NOT EXISTS Foods( 
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    description text NULL
 );
-CREATE TABLE `Food Analytical Results`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `food_id` BIGINT NOT NULL,
-    `analysis method` VARCHAR(255) NOT NULL,
-    `QC` VARCHAR(255) NOT NULL,
-    `mean` BIGINT NOT NULL,
-    `N` BIGINT NULL,
-    `SEM` BIGINT NULL,
-    `Min` BIGINT NULL,
-    `Max` BIGINT NULL
+
+CREATE TABLE IF NOT EXISTS Labels(  /* For Food items classifications*/
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+    name varchar(255),  /* eg 'meat', 'fish/seafood', 'vegatable', 'dairy', 'herb', 'conditment', etc...*/
+    description varchar(255) NULL
 );
-ALTER TABLE
-    `Recipe-foods` ADD CONSTRAINT `recipe_foods_recipe_id_foreign` FOREIGN KEY(`recipe_id`) REFERENCES `Recipes`(`id`);
-ALTER TABLE
-    `Condition Related Substances` ADD CONSTRAINT `condition related substances_condition_id_foreign` FOREIGN KEY(`condition_id`) REFERENCES `Physical Conditions`(`id`);
-ALTER TABLE
-    `Recipes` ADD CONSTRAINT `recipes_updated_by_foreign` FOREIGN KEY(`updated_by`) REFERENCES `Users`(`id`);
-ALTER TABLE
-    `User-Allergies` ADD CONSTRAINT `user_allergies_allergen_id_foreign` FOREIGN KEY(`allergen_id`) REFERENCES `Substances`(`id`);
-ALTER TABLE
-    `User-Conditions` ADD CONSTRAINT `user_conditions_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
-ALTER TABLE
-    `user-watchlists` ADD CONSTRAINT `user_watchlists_recipe_id_foreign` FOREIGN KEY(`recipe_id`) REFERENCES `Recipes`(`id`);
-ALTER TABLE
-    `Substances` ADD CONSTRAINT `substances_id_foreign` FOREIGN KEY(`id`) REFERENCES `Dietary Reference Intakes (DRI)`(`nutrient_code`);
-ALTER TABLE
-    `Condition Related Substances` ADD CONSTRAINT `condition related substances_sub_id_foreign` FOREIGN KEY(`sub_id`) REFERENCES `Substances`(`id`);
-ALTER TABLE
-    `Recipe-foods` ADD CONSTRAINT `recipe_foods_food_id_foreign` FOREIGN KEY(`food_id`) REFERENCES `Food Items`(`id`);
-ALTER TABLE
-    `user-watchlists` ADD CONSTRAINT `user_watchlists_uid_foreign` FOREIGN KEY(`uid`) REFERENCES `Users`(`id`);
-ALTER TABLE
-    `Food Analytical Results` ADD CONSTRAINT `food analytical results_food_id_foreign` FOREIGN KEY(`food_id`) REFERENCES `Food Items`(`id`);
-ALTER TABLE
-    `Users` ADD CONSTRAINT `users_age_group_foreign` FOREIGN KEY(`age_group`) REFERENCES `Dietary Reference Intakes (DRI)`(`age_group_code`);
-ALTER TABLE
-    `Food-nutrients` ADD CONSTRAINT `food_nutrients_food_id_foreign` FOREIGN KEY(`food_id`) REFERENCES `Food Items`(`id`);
-ALTER TABLE
-    `User-Allergies` ADD CONSTRAINT `user_allergies_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
-ALTER TABLE
-    `Food-nutrients` ADD CONSTRAINT `food_nutrients_sub_id_foreign` FOREIGN KEY(`sub_id`) REFERENCES `Substances`(`id`);
-ALTER TABLE
-    `Users` ADD CONSTRAINT `users_gender_foreign` FOREIGN KEY(`gender`) REFERENCES `Dietary Reference Intakes (DRI)`(`gender_code`);
-ALTER TABLE
-    `References` ADD CONSTRAINT `references_ref_id_foreign` FOREIGN KEY(`ref_id`) REFERENCES `Result-references`(`ref_id`);
-ALTER TABLE
-    `Result-references` ADD CONSTRAINT `result_references_result_id_foreign` FOREIGN KEY(`result_id`) REFERENCES `Food Analytical Results`(`id`);
-ALTER TABLE
-    `User-Conditions` ADD CONSTRAINT `user_conditions_condition_id_foreign` FOREIGN KEY(`condition_id`) REFERENCES `Physical Conditions`(`id`);
-ALTER TABLE
-    `Recipes` ADD CONSTRAINT `recipes_autor_foreign` FOREIGN KEY(`autor`) REFERENCES `Users`(`id`);
+
+CREATE TABLE IF NOT EXISTS Food_Labels( /* Combined table for Foods and Labels */
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+    foodId INT, 
+    labelId INT, 
+    FOREIGN KEY (foodId) REFERENCES Foods(id),
+    FOREIGN KEY (labelId) REFERENCES Labels(id),
+    PRIMARY KEY (foodId, labelId)
+);
+
+CREATE TABLE IF NOT EXISTS Food_Nutrients( /* Combined table for Foods and Substances */
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+    foodId INT, 
+    subsId INT, 
+    FOREIGN KEY (foodId) REFERENCES Foods(id),
+    FOREIGN KEY (subsId) REFERENCES Substances(id),
+    PRIMARY KEY (foodId, subsId)
+);
+
+CREATE TABLE IF NOT EXISTS Recipes(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    description TEXT,
+    createDate TIMESTAMP,
+    last_update TIMESTAMP NULL,
+    author INT,
+    updatedBy INT,
+    FOREIGN KEY (author) REFERENCES Users(id),
+    FOREIGN KEY (updatedBy) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS User_Watchlists( /* Combined table of Recipes and Users*/ 
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+	uid INT, 
+    recipeId INT,
+	FOREIGN KEY (uid) REFERENCES Users(id),
+    FOREIGN KEY (recipeId) REFERENCES Recipes(id),
+    PRIMARY KEY (uid, recipeId)
+);
+
+CREATE TABLE IF NOT EXISTS Recipe_Labels( /* Combined table of Recipes and DietPreferences*/ 
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+    recipeId INT,
+    dietId INT, 
+    FOREIGN KEY (recipeId) REFERENCES Recipes(id),
+    FOREIGN KEY (dietId) REFERENCES DietPreferences(id),
+    PRIMARY KEY (recipeId, dietId)
+);
+
+CREATE TABLE IF NOT EXISTS Recipe_Foods( /* Combined table of Recipes and Foods*/ 
+	id INT AUTO_INCREMENT, 
+    UNIQUE(id),
+    recipeId INT,
+    FoodId INT,
+    quantity FLOAT,
+    unit INT,
+    FOREIGN KEY (recipeId) REFERENCES Recipes(id),
+    FOREIGN KEY (FoodId) REFERENCES Foods(id),
+    FOREIGN KEY (unit) REFERENCES Units(id),
+    PRIMARY KEY (recipeId, FoodId)
+);
+
+-- CREATE TABLE IF NOT EXISTS FoodAnalyticalResults( /* Likely wont be used*/
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     foodId INT,
+--     analyasis_method VARCHAR(100), 
+--     QC VARCHAR(100),
+--     unit INT,
+--     mean FLOAT, 
+--     n INT NULL, 
+--     sem INT NULL,
+--     min INT NULL,
+--     max INT NULL,
+--     FOREIGN KEY (unit) REFERENCES Units(id),
+--     FOREIGN KEY (foodId) REFERENCES Foods(id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS LiteraryReferences( /* Likely wont be used*/
+-- 	refId INT PRIMARY KEY AUTO_INCREMENT, 
+--     source_name VARCHAR(255),
+--     first_author VARCHAR(100),
+--     publication VARCHAR (255),
+--     publish_month DATE NULL, 
+--     publish_year YEAR
+-- );
+
+-- CREATE TABLE IF NOT EXISTS Result_References( /* Likely wont be used*/
+-- 	id INT AUTO_INCREMENT, 
+--     UNIQUE(id),
+--     resultId INT, 
+--     refId INT, 
+--     FOREIGN KEY (resultId) REFERENCES (FoodAnalyticalResults),
+-- 	FOREIGN KEY (refId) REFERENCES (LiteraryReferences),
+--     PRIMARY KEY (resultId, refId)
+-- );
+
